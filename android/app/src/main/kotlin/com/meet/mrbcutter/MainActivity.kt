e3ba6739748cbd1a2facee6e0f026655b669b15f
Mrb-Official <mrb40315@gmail.com>
2026-04-26 20:33:59 +0530

    main activtiy fix

--- android/app/src/main/kotlin/com/meet/mrbcutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/meet/mrbcutter/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.meet.mrbcutter
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        // Ye line saare plugins ko zabardasti native engine se jod degi
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
